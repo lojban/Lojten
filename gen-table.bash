2ee99ff2c858ad5a873a316a312f3ae87a069fb4
= <roman_naumann@fastmail.fm>
2009-10-17 12:44:14 +0200

    no longer depends on "clj"

--- gen-table.bash
+++ gen-table.bash
@@ -3,7 +3,7 @@ echo "removing old classfiles, if any"
 rm -r classes
 mkdir classes
 echo "generating new classfiles"
-echo "(compile 'lojten)" | clj -cp classes
+echo "(compile 'lojten)" | java -cp ./clojure.jar:./classes:. clojure.lang.Repl
 echo "generating table"
 java -cp ./clojure.jar:./classes lojten --gen > lojten.mim
 echo "clearing up"
