8c47273bb5cf2bf1795be988bc04fd22a4b57b41
= <roman_naumann@fastmail.fm>
2009-10-17 11:58:42 +0200

    "classes" dir will be created and removed now after the build process

--- gen-table.bash
+++ gen-table.bash
@@ -1,7 +1,10 @@
 #/bin/bash
-echo "removing old classfiles"
-rm classes/*
+echo "removing old classfiles, if any"
+rm -r classes
+mkdir classes
 echo "generating new classfiles"
 echo "(compile 'lojten)" | clj -cp classes
 echo "generating table"
 java -cp ./clojure.jar:./classes lojten --gen > lojten.mim
+echo "clearing up"
+rm -r classes
